1fd022c068d083c37da4e3baeaff57d4e463e992
Amandeep Rikhi <amandeeprikhi@gmail.com>
2019-07-29 09:08:07 +0530

    refactoring

---
+++ utils/hashing.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcryptjs');
+const { SALT } = require('../config/config');
+
+exports.makehash = async (password) => {
+    try {
+        return bcrypt.hashSync(password, SALT);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+exports.compareHash = async(enteredPassword, storedPassword) => {
+    try {
+        return bcrypt.compareSync(enteredPassword, storedPassword);
+    } catch (err) {
+        console.log(err);
+    }
+}
---
+++ utils/token.js
@@ -0,0 +1,35 @@
+let jwt = require("jsonwebtoken");
+const { SECRET } = require("../config/config");
+
+exports.generateToken = async (email, data) => {
+  try {
+    tokendata = {
+      email: email,
+      isDoctor: clientdata.isDoctor,
+      isActive: clientdata.isActive
+    };
+    return jwt.sign({ tokendata }, SECRET, {
+      expiresIn: "24h" /* expires in 24 hours */
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.extractToken = async (req) => {
+  try {
+    let tokenHeaders = ["x-access-token", "authorization"];
+
+    let token = "";
+
+    for (let tokenHeader of tokenHeaders) {
+      if (tokenHeader in req.headers) {
+        token = req.headers[tokenHeader];
+      }
+    }
+
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
